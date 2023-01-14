3fd136494169d954a0b7c7cd8639ae1e98c8c559
Deb Prakash Chatterjee <shuvamconcepts28@gmail.com>
2023-01-15 00:18:39 +0530

    Added code for Single Number

---
+++ Single Number/Code.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int len = nums.length, ans = 0;
+        for(int i = 0; i < len; i++) ans = ans ^ nums[i];
+        return ans;
+    }
+}
---
+++ Single Number/README.md
@@ -0,0 +1,13 @@
+# Approach - 
+
+It is the property of XOR, that
+0 ^ 0 = 0
+0 ^ 1 = 1
+1 ^ 0 = 1
+1 ^ 1 = 1
+
+Applied the same principle.
+
+Example - [2,2,1] --> 010 ^ 010 ^ 001 = 001
+
+Happy Coding :)
